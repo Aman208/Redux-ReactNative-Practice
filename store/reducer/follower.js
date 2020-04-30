a7ca21bd6d62f200e574e37a5aa4868044409907
Aman208 <amankumar20804@gmail.com>
2020-05-01 02:48:10 +0530

    dummy data added

--- store/reducer/follower.js
+++ store/reducer/follower.js
@@ -12,9 +12,29 @@ var intialState = {
         "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
       subtitle: "Vice Chairman",
     },
+    { 
+      id: "f3",
+      name: "Aman Keshri",
+      avatar_url:"https://images.pexels.com/photos/462030/pexels-photo-462030.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" ,
+      subtitle: "Principal",
+    } ,
+    { id: "f4",
+    name: "Dhruv Bhaat",
+      avatar_url:"https://www.gettyimages.com/gi-resources/images/500px/983794168.jpg" ,
+      subtitle:"Director"
+    }
+
   ],
 
-  following: [],
+  following: [
+
+    {id: "f5",
+    name: "Niraj Singh",
+      avatar_url:"https://killerattitudestatus.in/wp-content/uploads/2019/12/gud-night-images.jpg" ,
+      subtitle:"Employees"
+  }
+
+  ],
 };
 
 function followerReducer(state = intialState, action) {
